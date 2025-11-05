20b9e40c50ba7a340f1837be3c347d411282e0ab
Anjali <kushanjali838@gmail.com>
2025-11-05 07:26:10 +0000

    implementing variables

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,12 @@
 # Creating ec2 vm
 resource "aws_instance" "example" {
-  ami           = "ami-0a25a306450a2cba3"
-  instance_type = "t2.nano"
-  key_name      = "roche-key"
+  # ami = "ami-0a25a306450a2cba3"
+  ami = var.anjali-ami-id
+  instance_type = var.vm-size
+  key_name      = var.ec2-key-name
   # changing tags_all to tags
   tags = {
-    "Name" = "anjali-vm-1"
+    "Name" = var.vm-name
   }
 }
 
---
+++ my-varvalues.tfvars
@@ -0,0 +1,4 @@
+vm-name = "anjali-vmnew-1"
+vm-size = "t2.nano"
+ec2-key-name = "roche-key"
+anjali-ami-id = "ami-0a25a306450a2cba3"
